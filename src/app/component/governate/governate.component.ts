91559ccb782fe39ac63e08f9e54e6cb4e5ae0339
gehad-abdelmonem <gehadabdelmonam@gmail.com>
2023-06-14 12:42:02 +0300

    apply loading spinner on governerate component and improve the performance

--- src/app/component/governate/governate.component.ts
+++ src/app/component/governate/governate.component.ts
@@ -11,16 +11,24 @@ import { AngularMateralService } from 'src/app/services/angular-materal.service'
 })
 export class GovernateComponent implements OnInit {
   governateList: IGovernarate[] = [];
-
+  isLoading = true;
+  isError = false;
   constructor(
     private router:Router,
     private GovernorateService:GovernorateService,
     private AngularMateralService:AngularMateralService
     ){}
   ngOnInit(): void {
-    this.GovernorateService.GetAll().subscribe((data) => {
-      this.governateList = data;
-    });
+    this.GovernorateService.GetAll().subscribe({
+      next:(data)=>{
+        this.isLoading = false;
+        this.governateList = data;
+      },
+      error:(e)=>{
+        this.isLoading = false;
+        this.isError = true;
+      }
+    })
   }
   deleteGovernate(code:number)
   {
@@ -28,10 +36,15 @@ export class GovernateComponent implements OnInit {
     .afterClosed().subscribe(resp=>{
       if(resp)
       {
-        this.GovernorateService.Delete(code).subscribe(resp=>{
+        this.GovernorateService.Delete(code).subscribe({
+          next:(resp)=>{
             this.ngOnInit();
             this.AngularMateralService.addAndUpdateSuccess("! Deleted Successfuly");
-            });
+            },
+          error:(e)=>{
+            this.AngularMateralService.addAndUpdateSuccess("An Error Occured. Try Again Later !");
+          }  
+        });
       }
     })
   }
