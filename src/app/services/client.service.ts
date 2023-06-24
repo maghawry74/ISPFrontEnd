402db5e6c96486354c427a978bdaba4a02f0e5b4
Maghawry H <maghawry.hussein20@gmail.com>
2023-06-25 02:49:23 +0300

    Modifying Bill Urls

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -2,30 +2,35 @@ import { Injectable } from '@angular/core';
 import { GenericService } from './generic.service';
 import { IClient } from '../models/IClient';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { IBill } from '../models/IBill';
-
+import { environment } from 'src/environment/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class ClientService extends GenericService<IClient, string> {
+  BillURL = '';
   constructor(httpClient: HttpClient) {
     super('client', httpClient);
+    this.BillURL = `${environment.APIURL}/bill`;
   }
 
   GetBills(data: { Ssid: string; Condition: boolean }) {
     return this.client.get<IBill[]>(
-      `${this.Url}/bill?ssid=${data.Ssid}&condition=${data.Condition}`
+      `${this.BillURL}/clientbills?ssid=${data.Ssid}&condition=${data.Condition}`
     );
   }
 
   GetNextBill(data: { ssid: string; Month: number }) {
     return this.client.get<IBill>(
-      `${this.Url}/nextBill?ssid=${data.ssid}&monthNumber=${data.Month}`
+      `${this.BillURL}/next-month-bill?ssid=${data.ssid}&monthNumber=${data.Month}`
     );
   }
 
   PayBill(data: { ssid: string; id: number }) {
-    return this.client.patch(`${this.Url}/bill`, data, this.headersOptions);
+    return this.client.put(
+      `${this.BillURL}/paybill`,
+      data,
+      this.headersOptions
+    );
   }
 }
